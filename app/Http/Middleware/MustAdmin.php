5035df9e2d8a18716c16e5d360e547fe6e79a3b6
Raffi Ihza Zuhairnawan <115229257+raffiihza@users.noreply.github.com>
2025-02-15 14:54:49 +0000

    Add admin and guru role

---
+++ app/Http/Middleware/MustAdmin.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
+
+class MustAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        //bisa melewati middleware jika role = Admin
+        if (!Auth::check()) {
+            abort(404);
+        } elseif (Auth::user()->role == 'Admin') {
+            return $next($request);
+        } else {
+            abort(404);
+        }
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -7,14 +7,20 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::middleware(['auth','admin'])->group(function () {
+    Route::get('/users', function () {
+        return view('admins.users');
+    })->name('users');
 });
 
 require __DIR__.'/auth.php';
