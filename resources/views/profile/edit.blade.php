f23df81abe7bfe4645565fe55da62cd297e4d48f
Raffi Ihza Zuhairnawan <115229257+raffiihza@users.noreply.github.com>
2025-02-16 09:13:49 +0000

    fix: profile box size

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -3,9 +3,9 @@
 
 @section('content')
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+    <div>
+        <div class="space-y-6">
+            <div class="bg-white p-6 shadow rounded-lg">
                 <div class="max-w-xl">
                     @include('profile.partials.update-profile-information-form')
                 </div>
